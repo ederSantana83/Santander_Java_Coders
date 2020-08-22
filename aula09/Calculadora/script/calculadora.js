7aed6141584976510a880a337a045e25a84fda52
ederSantana83 <edersana@gmail.com>
2020-08-22 10:49:19 -0300

    Enviada aula09

---
+++ aula09/Calculadora/script/calculadora.js
@@ -0,0 +1,36 @@
+function calcular(){
+var operacao = document.getElementById("operacao").value;
+var num1 = document.getElementById("num1").value;
+var num2 = document.getElementById("num2").value;
+
+if(num1 == '' || num1 == null){
+    alert('Favor digitar um número válido para operação (num1)');
+    return false;
+}
+if(num2 == '' || num2 == null){
+    alert('Favor digitar um número válido para operação (num2)');
+    return false;
+}
+var resultado = null;
+num1 = parseFloat(num1);
+num2 = parseFloat(num2);
+
+switch(operacao){
+    case '1':
+        resultado = num1 - num2;
+        break;
+    case '2':
+        resultado = num1 + num2;
+        break;
+    case '3':
+        resultado = num1 * num2;
+        break;
+    case '4':
+        resultado = num1 / num2;
+        break;   
+    default:
+        alert('A operação selecionada é inválida');
+        return false;     
+}
+    document.getElementById('resultado').value = resultado;
+}
---
+++ aula09/Tarefas - Eventos/scripts/script.js
@@ -0,0 +1,10 @@
+function boasVindas(){
+alert("Você clicou no botão!")
+}
+/*
+		<!-- <div ondblclick=boasVindas() style="background: #CCC; height: 150px; width: 150px;"></div> -->
+		<!-- <div onmousedown=boasVindas() style="background: #CCC; height: 150px; width: 150px;"></div> -->
+		<!-- <div onmouseout=boasVindas() style="background: #CCC; height: 150px; width: 150px;"></div> -->
+		<!-- <div onmouseover=boasVindas() style="background: #CCC; height: 150px; width: 150px;"></div> -->
+
+*/
